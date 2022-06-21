b75704de0c9b5c69c12d0c137aec6cd772088559
Mark Roberts <marrober@redhat.com>
2022-06-21 17:13:06 +0100

    Update of test script

--- test.sh
+++ test.sh
@@ -1,10 +1,16 @@
 echo ""
 echo ""
 echo "Test the development environment"
-curl -k $(oc get route -n myapp-development -o jsonpath='{"http://"}{.items[0].spec.host}{"/System/propertiesJavaHome"}') 
+curl -k http://myapp-route-myapp-development.apps.skylake.demolab.local/System/propertiesJavaHome
 
 echo ""
 echo ""
 echo "Test the QA environment"
 echo ""
-curl -k $(oc get route -n myapp-qa -o jsonpath='{"http://"}{.items[0].spec.host}{"/System/propertiesJavaHome"}') 
+curl -k http://myapp-route-myapp-qa.apps.skylake.demolab.local/System/propertiesJavaHome
+
+echo ""
+echo ""
+echo "Test the Production environment"
+echo ""
+curl -k http://myapp-route-myapp-production.apps.celeron.demolab.local/System/propertiesJavaHome
